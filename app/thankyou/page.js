58ede4fef120b9fb869cf9b6d416ad2d51e2aa8a
Tom <tbvanreijn@gmail.com>
2024-04-22 15:02:25 +0200

    thankyou

--- app/thankyou/page.js
+++ app/thankyou/page.js
@@ -3,6 +3,23 @@ import React from "react";
 function page() {
   return (
     <div>
+      <html>
+        <script
+          type="text/javascript"
+          src="https://app.paypro.nl/pat/pat.js"
+          data-campaign-id="24939"
+          data-order-amount="AMOUNT_HERE" // Required when using variable commission
+          data-reference="REFERENCE_HERE" // Optional, but can be useful for administration purposes, or avoid duplicate conversions
+        ></script>
+        <noscript>
+          <img
+            src="https://app.paypro.nl/pat/pat.gif?campaignId=24939&orderAmount=AMOUNT_HERE&reference=REFERENCE_HERE"
+            height="0"
+            style="display: none; visibility: hidden;"
+            width="0"
+          />
+        </noscript>
+      </html>
       <div className="bg-white p-6  md:mx-auto">
         <svg
           viewBox="0 0 24 24"

628f05eb4cd8c36164efde356f88f21f84797141
Tom <tbvanreijn@gmail.com>
2024-05-15 06:37:01 +0200

    html error

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({ children }: any) {
   return (
     <html lang="en">
       {" "}
-      <div className="[text-align:-webkit-center]">Centered Text</div>
       <Script
         type="text/javascript"
         src="https://app.paypro.nl/pat/pat.js"

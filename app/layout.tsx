ba3805d82cdacaa85c93a854b4471dac00968684
Tom <tbvanreijn@gmail.com>
2024-05-15 09:59:35 +0200

    nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,6 @@ export default function RootLayout({ children }: any) {
               <div className="flex w-full px-4 lg:px-40 py-4 items-center border-b text-center gap-8 justify-between h-[69px]" />
             }>
             {" "}
-            <Navbar />
           </Suspense>
         </section>
         <main className="flex flex-1 flex-col items-center py-16">

50cc5c1fcc92a648bcc06d6ade5a405619bebde9
Tom <tbvanreijn@gmail.com>
2024-05-15 09:42:30 +0200

    width

--- app/page.tsx
+++ app/page.tsx
@@ -22,38 +22,36 @@ export default async function Index() {
   }
 
   return (
-    <main>
-      <div className="flex flex-col items-center w-full">
-        <div className="flex flex-col lg:flex-row items-center gap-8 p-8 max-w-6xl w-full">
-          <div className="flex flex-col space-y-4 lg:w-1/2 w-full">
-            <h1 className="text-5xl font-bold ">
-              Professionele fotosessie <br /> met behulp van AI✨
-            </h1>
-            <p className="text-gray-600 text-xl">
-              Vergeet dure studio's en ongemakkelijke fotosessies! <br />
-              Genereer professionele zakelijke portetfoto's in HD studio
-              kwaliteit die veel goedkoper zijn en geen tijd en reizen in beslag
-              nemen, makkelijk vanaf thuis of je werk.
-            </p>
-            <div className="flex flex-col space-y-2">
-              <Link href="/login">
-                <Button className="border rounded-full  h-12 px-6 m-2 text-lg text-white bg-sky-600 hover:bg-sky-700 w-full lg:w-1/2">
-                  Begin jouw fotosessie{" "}
-                </Button>
-              </Link>
-            </div>
-          </div>
-          <div className="lg:w-1/2 w-full mt-8 lg:mt-0">
-            <img
-              src={hero2.src}
-              alt="AI Headshot Illustration"
-              className="rounded-lg object-cover w-full h-full"
-            />
+    <div className="flex flex-col items-center pt-16">
+      <div className="flex flex-col lg:flex-row items-center gap-8 p-8 max-w-6xl w-full">
+        <div className="flex flex-col space-y-4 lg:w-1/2 w-full">
+          <h1 className="text-5xl font-bold ">
+            Professionele fotosessie <br /> met behulp van AI✨
+          </h1>
+          <p className="text-gray-600 text-xl">
+            Vergeet dure studio's en ongemakkelijke fotosessies! <br />
+            Genereer professionele zakelijke portetfoto's in HD studio kwaliteit
+            die veel goedkoper zijn en geen tijd en reizen in beslag nemen,
+            makkelijk vanaf thuis of je werk.
+          </p>
+          <div className="flex flex-col space-y-2">
+            <Link href="/login">
+              <Button className="border rounded-full  h-12 px-6 m-2 text-lg text-white bg-sky-600 hover:bg-sky-700 w-full lg:w-1/2">
+                Begin jouw fotosessie{" "}
+              </Button>
+            </Link>
           </div>
         </div>
-        <ExplainerSection />
-        <PricingSection /> <Faq />
+        <div className="lg:w-1/2 w-full mt-8 lg:mt-0">
+          <img
+            src={hero2.src}
+            alt="AI Headshot Illustration"
+            className="rounded-lg object-cover w-full h-full"
+          />
+        </div>
       </div>
-    </main>
+      <ExplainerSection />
+      <PricingSection /> <Faq />
+    </div>
   );
 }
